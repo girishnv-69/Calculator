58b05fe2887d19fc5dcfa6a10258a67944e03580
girishnv-69 <girishgiri246@gmail.com>
2023-06-03 11:37:13 +0530

    first commit

---
+++ src/Calculator.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Calculator!");
+        System.out.println("Enter two numbers:");
+
+        System.out.print("Number 1: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Number 2: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Select an operation:");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+
+        System.out.print("Enter your choice (1-4): ");
+        int choice = scanner.nextInt();
+
+        double result = 0.0;
+        boolean validChoice = true;
+
+        switch (choice) {
+            case 1:
+                result = num1 + num2;
+                break;
+            case 2:
+                result = num1 - num2;
+                break;
+            case 3:
+                result = num1 * num2;
+                break;
+            case 4:
+                if (num2 != 0)
+                    result = num1 / num2;
+                else {
+                    System.out.println("Error: Division by zero!");
+                    validChoice = false;
+                }
+                break;
+            default:
+                System.out.println("Invalid choice!");
+                validChoice = false;
+        }
+
+        if (validChoice)
+            System.out.println("Result: " + result);
+
+        scanner.close();
+    }
+}
+
